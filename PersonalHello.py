fe792aee287b60476c205aad32fc61d11732b1ce
meschelle7-cpu <meschelle7@gmail.com>
2026-06-07 10:11:55 -0400

    Update PersonalHello.py

--- PersonalHello.py
+++ PersonalHello.py
@@ -1,4 +1,10 @@
 #A personal hello
+
+#Prompt the user to enter their name
 name = input("What is your name? ")
+
+#Creates a personalized greeting
 greeting = "Hello " + name
+
+#Displays the greeting
 print(greeting)
